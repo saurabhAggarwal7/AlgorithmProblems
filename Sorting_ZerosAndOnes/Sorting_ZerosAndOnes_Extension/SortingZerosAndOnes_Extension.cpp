825570b2373accf5b9caede3146a1acf810f37f6
Saurabh Aggarwal <sorav77@gmail.com>
2017-05-16 00:40:08 +0530

    Checked

---
+++ Sorting_ZerosAndOnes/Sorting_ZerosAndOnes_Extension/SortingZerosAndOnes_Extension.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+//Global:
+using namespace std;
+
+void swap(int *start, int *end)
+{
+    int temp = *start;
+    *start = *end;
+    *end = temp;
+}
+
+int main()
+{
+	int arr[] = {0, 1, 2, 2, 1, 1, 0, 0, 2, 2, 1, 1};
+	int length = sizeof(arr)/sizeof(arr[0]);
+	int left_index = 0, right_index = length-1;
+	int lower_bound = 0, upper_bound = 2, middle_bound = 1;
+
+	//can also take length-1 for resolving using backward counter
+	int middle_index = 0; 
+
+	while (middle_index <= right_index)
+    {
+		//Both lower and middle started with 0, so both move forwarded, middle will be handled again to resolve middle & lower
+		if(arr[middle_index] == lower_bound)
+			swap(&arr[left_index++], &arr[middle_index++]);
+
+		if(arr[middle_index] == middle_bound)
+			middle_index ++;
+
+		if(arr[middle_index] == upper_bound)
+			swap(&arr[middle_index], &arr[right_index--]);
+    }
+
+	for (int i = 0; i < length; i++) {
+		cout<<arr[i];
+	}
+
+	getchar();
+	return 0;
+}
